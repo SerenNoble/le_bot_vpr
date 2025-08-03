5d7007ee55585da2b0ac6d858c8f270689e8ef45
ParticleG <particle_g@outlook.com>
2025-08-03 18:41:57 +0800

    - Update sources

--- app/main.py
+++ app/main.py
@@ -1,17 +1,23 @@
+import sys
+import os
+
+sys.path.append(os.path.join(os.path.dirname(os.path.dirname(__file__)), 'deps', 'vpr'))
+
 from deps.vpr.mvector.predict import MVectorPredictor
 from fastapi import FastAPI
 
 predictor = MVectorPredictor(configs='app/vpr/configs/eres2net.yml',
-                             model_path='models/CAMPPlus_Fbank/best_model/')
+                             model_path='app/vpr/models/ERes2Net_Fbank/',
+                             use_gpu=False)
 # 获取音频特征
-embedding = predictor.predict(audio_data='dataset/a_1.wav')
+embedding = predictor.predict(audio_data='deps/vpr/dataset/a_1.wav')
 # 获取两个音频的相似度
-similarity = predictor.contrast(audio_data1='dataset/a_1.wav', audio_data2='dataset/a_2.wav')
+similarity = predictor.contrast(audio_data1='deps/vpr/dataset/a_1.wav', audio_data2='deps/vpr/dataset/a_2.wav')
 
 # 注册用户音频
-predictor.register(user_name='夜雨飘零', audio_data='dataset/test.wav')
+predictor.register(user_name='夜雨飘零', audio_data='deps/vpr/dataset/b_1.wav')
 # 识别用户音频
-name, score = predictor.recognition(audio_data='dataset/test1.wav')
+name, score = predictor.recognition(audio_data='deps/vpr/dataset/b_2.wav')
 # 获取所有用户
 users_name = predictor.get_users()
 # 删除用户音频
